2a6a16ffa01d3724e7d2515d855593417e7c8a36
Psiant <119295992+Psiant@users.noreply.github.com>
2025-07-03 01:41:28 +0500

    Create cards.0c8914067a6e408f1ce3.js

---
+++ frontend/bild/cards.0c8914067a6e408f1ce3.js
@@ -0,0 +1,19 @@
+"use strict";
+(window.webpackChunk = window.webpackChunk || []).push([[76012], {
+    168258: (e, d, n) => {
+        n.r(d)
+    }
+}]);
+var _global = "undefined" != typeof window ? window : "undefined" != typeof global ? global : "undefined" != typeof self ? self : {};
+_global.SENTRY_RELEASE = {
+    id: "build_2025_07_01_16_55_40_release"
+},
+function() {
+    try {
+        var e = "undefined" != typeof window ? window : "undefined" != typeof global ? global : "undefined" != typeof self ? self : {}
+          , d = (new Error).stack;
+        d && (e._sentryDebugIds = e._sentryDebugIds || {},
+        e._sentryDebugIds[d] = "0068a944-e988-429a-9f90-060482e2d48b",
+        e._sentryDebugIdIdentifier = "sentry-dbid-0068a944-e988-429a-9f90-060482e2d48b")
+    } catch (e) {}
+}();
